5098652e4fc082aeec96f74f69824b5a0ab17dea
Steve Massey <stevenrmassey@gmail.com>
2012-03-12 23:34:04 -0500

    google analytics added to tags.php

--- tags.php
+++ tags.php
@@ -25,11 +25,28 @@ else
 ?>
 
 <html>
-    <title>lostorbit.net</title>
-    <link href="style.css" rel="stylesheet" type="text/css" />
-    <style>
-        body {background-color:#000;  background-image:url('images/rings1.jpg'); background-repeat:repeat-x; }
-    </style>
+    <head>
+        <title>lostorbit.net</title>
+        <link href="style.css" rel="stylesheet" type="text/css" />
+        <style>
+            body {background-color:#000;  background-image:url('images/rings1.jpg'); background-repeat:repeat-x; }
+        </style>
+        <script type="text/javascript">
+
+          var _gaq = _gaq || [];
+          _gaq.push(['_setAccount', 'UA-3496022-1']);
+          _gaq.push(['_trackPageview']);
+
+          (function() {
+            var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+            ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+            var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+          })();
+
+        </script>
+
+
+    </head>
 <body>
 
 <div id="content">
